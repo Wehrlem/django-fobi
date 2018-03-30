027d2ad596193d7b67e5b9736967204d5164290f
Darko Zelenika <zelenika.darko@gmail.com>
2018-03-30 12:21:42 +0200

    changed version to 0.12.0.1

--- src/fobi/__init__.py
+++ src/fobi/__init__.py
@@ -1,5 +1,5 @@
 __title__ = 'django-fobi'
-__version__ = '0.12'
+__version__ = '0.12.0.1'
 __build__ = 0x000089
 __author__ = 'Artur Barseghyan <artur.barseghyan@gmail.com>'
 __copyright__ = '2014-2017 Artur Barseghyan'
